7528a49331dffe0d4a74856e3b9bc36ab8de1e7c
DakshalP <dakshal29@gmail.com>
2021-07-26 22:35:10 -0400

    add time based sorting

--- src/components/meetings/MeetingCardGroup.js
+++ src/components/meetings/MeetingCardGroup.js
@@ -3,6 +3,35 @@ import MeetingCard from './MeetingCard';
 import { Card, Divider, Segment } from 'semantic-ui-react';
 import styles from '../../styles/meetings.module.scss';
 
+//convert to comparable numbers [HH, MM]
+const convertTime = (time) => {
+    const period = time.match(/(AM|PM)/g)[0];
+    time = time.replace(/(AM|PM)/g, ''); //remove period
+
+    let timeArr = time.split(':').map((t) => parseInt(t));
+    if (period === 'PM') timeArr[0] += 12;
+
+    return timeArr;
+};
+
+const compareTimes = (timeA, timeB) => {
+    //empty cases
+    if (timeA === '' && timeB === '') return 0;
+    else if (timeA === '') return -1;
+    else if (timeB === '') return 1;
+
+    //compare times
+    const a = convertTime(timeA),
+        b = convertTime(timeB);
+
+    const hourDifference = Math.abs(a[0] - b[0]);
+    const secondDifference = Math.abs(a[1] - b[1]);
+
+    if (hourDifference > 0) return hourDifference;
+    else if (secondDifference > 0) return secondDifference / 60;
+    else return 0;
+};
+
 const MeetingCardGroup = ({
     header,
     meetings,
@@ -17,9 +46,15 @@ const MeetingCardGroup = ({
             </Divider>
         ) : null}
         <Card.Group centered={!isSecondary}>
-            {meetings.map((meeting) => (
-                <MeetingCard key={meeting._id} onEdit={onEdit} {...meeting} />
-            ))}
+            {meetings
+                .sort((a, b) => compareTimes(a.time, b.time))
+                .map((meeting) => (
+                    <MeetingCard
+                        key={meeting._id}
+                        onEdit={onEdit}
+                        {...meeting}
+                    />
+                ))}
         </Card.Group>
     </Segment>
 );
